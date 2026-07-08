0671fff13e339c7c4e428a3320bc794a7930955d
Ajnewlearner <aj207454@gmail.com>
2026-07-09 03:38:17 +0530

    day 14

---
+++ trappedWater.java
@@ -0,0 +1,34 @@
+class trappedWater {
+    public int trap(int[] height) {
+        int n = height.length;
+
+        int left = 0;
+        int right = n - 1;
+
+        int maxLeft = 0;
+        int maxRight = 0;
+
+        int totalWater = 0;
+
+        while (left <= right) {
+            if (height[left] <= height[right]) {
+                if (height[left] >= maxLeft) {
+                    maxLeft = height[left];
+                } else {
+                    totalWater += maxLeft - height[left];
+                }
+                left++; // Move left pointer to the right
+            } else {
+                if (height[right] >= maxRight) {
+                    maxRight = height[right];
+                } else {
+                    totalWater += maxRight - height[right];
+                }
+                right--; // Move right pointer to the left
+            }
+        }
+
+        // Return total trapped water
+        return totalWater;
+    }
+}
