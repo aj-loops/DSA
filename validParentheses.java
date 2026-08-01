1b5b255c2428adf7c394386ff3e7e232e98536e0
Ajnewlearner <aj207454@gmail.com>
2026-08-02 03:29:59 +0530

    day 19

---
+++ validParentheses.java
@@ -0,0 +1,21 @@
+
+import java.util.Stack;
+
+class validParentheses {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                st.push(')');
+            } else if (c == '{') {
+                st.push('}');
+            } else if (c == '[') {
+                st.push(']');
+            } else {
+                if (st.isEmpty() || st.pop() != c)
+                    return false;
+            }
+        }
+        return st.isEmpty();
+    }
+}
