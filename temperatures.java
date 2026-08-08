f5bd2121487553ee75aec4a5d725969b1832bb9a
Ajnewlearner <aj207454@gmail.com>
2026-08-08 22:53:57 +0530

    day21

--- temperatures.java
+++ temperatures.java
@@ -1,25 +1,38 @@
-import java.util.Stack;
+// import java.util.Stack;
 
 class temperatures {
-    public int[] dailyTemperatures(int[] temperatures) {
+    // public int[] dailyTemperatures(int[] temperatures) {
 
-        int n = temperatures.length;
+    // int n = temperatures.length;
+    // int[] ans = new int[n];
+
+    // Stack<Integer> stack = new Stack<>();
+
+    // for (int i = 0; i < n; i++) {
+
+    // while (!stack.isEmpty() &&
+    // temperatures[i] > temperatures[stack.peek()]) {
+
+    // int prev = stack.pop();
+    // ans[prev] = i - prev;
+    // }
+
+    // stack.push(i);
+    // }
+
+    // return ans;
+    // }
+    public int[] dailyTemperatures(int[] temp) {
+        int n = temp.length;
         int[] ans = new int[n];
-
-        Stack<Integer> stack = new Stack<>();
-
-        for (int i = 0; i < n; i++) {
-
-            while (!stack.isEmpty() &&
-                    temperatures[i] > temperatures[stack.peek()]) {
-
-                int prev = stack.pop();
-                ans[prev] = i - prev;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (temp[i] < temp[j]) {
+                    ans[i] = j - i;
+                    break;
+                }
             }
-
-            stack.push(i);
         }
-
         return ans;
     }
 }
